cfc300f85999605e942ff77e76e5e9e559cc4401
Nate <nate@qstreet.org>
2025-07-14 14:48:15 +0300

    Update script

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# mac-reset
-
-```
-curl -sL https://raw.githubusercontent.com/NaeNate/mac-reset/refs/heads/main/reset.sh | bash
-```
+# Mac Reset
--- reset.sh
+++ reset.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew install fish fisher pnpm git
+brew install fish fisher git pnpm ffmpeg imagemagick yt-dlp
 brew install --cask 1password firefox visual-studio-code
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
-fish -c "set -U fish_greeting"
 
 mkdir -p ~/.config/fish/functions
-echo -e 'function fish_prompt\n    set_color $fish_color_cwd\n    echo -n (prompt_pwd)\n    set_color normal\n    echo -n " => "\nend' > ~/.config/fish/functions/fish_prompt.fish
-echo -e 'function sl\n    ls -A1 $argv\nend' > ~/.config/fish/functions/sl.fish
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fish -c 'set -U fish_greeting'
+fish -c 'fisher install jorgebucaran/nvm.fish && nvm install lts && set -U nvm_default_version (nvm current)'
+echo -e "function fish_prompt\n    set_color $fish_color_cwd\n    echo -n (prompt_pwd)\n    set_color normal\n    echo -n ' => '\nend" > ~/.config/fish/functions/fish_prompt.fish
+echo -e 'function sl; ls -A1 $argv; end' > ~/.config/fish/functions/sl.fish
+echo -e 'function ru; rm -rf .cache .lesshst .npm .profile .viminfo .zsh_history .zsh_sessions .zshenv .DS_Store .claude.json.backup; end' > ~/.config/fish/functions/ru.fish
 
 mkdir ~/code
 touch ~/.hushlogin
